5b7e76e1f0a1b6c399830637048ecf8aad6fe316
TkWarrior <talibulhaque369@gmail.com>
2025-10-03 19:50:48 +0530

    added custome middleware

--- server/server.js
+++ server/server.js
@@ -5,9 +5,19 @@ const menue = require("../model/menue");
 
 const app = express();
 app.use(express.json());
+
+// custom middleware
+const logRequest = function(req , res ,next){
+    console.log(`[${new Date().toLocaleString()}] : Request made to  ${req.originalUrl}`)
+    next();
+}
+
+app.use(logRequest)
+
 app.get("/", function (req, res) {
   res.send("hello frontend");
 });
+
 const personRouter = require("../router/personRouter");
 app.use("/" , personRouter)
 const menueRouter = require("../router/menuerouter");
