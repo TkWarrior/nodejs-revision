1624004fb3721d1e2940bf0c914d8136aa443043
TkWarrior <talibulhaque369@gmail.com>
2025-10-03 18:45:35 +0530

    moved sensitive info to .env file

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
 // Set up the mongo db connection
-DATABASE_URL = "mongodb://127.0.0.1:27017/users";
-mongoose.connect(DATABASE_URL, {
+
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
